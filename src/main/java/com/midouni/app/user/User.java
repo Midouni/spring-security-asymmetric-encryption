9ea9ff974f9771ea292cdd8ddef8f1a3666db288
Midouni <mohamedmidouni2@gmail.com>
2025-10-23 21:45:54 +0100

    Build User Entity

--- src/main/java/com/midouni/app/user/User.java
+++ src/main/java/com/midouni/app/user/User.java
@@ -1,4 +1,106 @@
 package com.midouni.app.user;
 
-public class User {
+import com.midouni.app.common.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "USER")
+@EntityListeners(AuditingEntityListener.class)
+public class User extends BaseEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "FIRST_NAME", nullable = false)
+    private String firstName;
+    @Column(name = "LAST_NAME", nullable = false)
+    private String lastName;
+
+    @Column(name = "EMAIL", nullable = false, unique = true)
+    private String email;
+
+    @Min(value = 8)
+    @Column(name = "PASSWORD",nullable = false)
+    private String password;
+
+    @Column(name = "PHONE_NUMNER", nullable = false,unique = true)
+    private String phoneNumber;
+
+    @Column(name = "BIRTHDAY")
+    private LocalDate birthday;
+
+    @Column(name = "ENABLED")
+    private boolean enabled;
+
+    @Column(name = "IS_ACCOUNT_LOCKED")
+    private boolean locked;
+
+    @Column(name = "IS_CREDENTIAL_EXPIRED")
+    private boolean credentialsExpired;
+
+    @Column(name = "IS_ACCOUNT_EXPIRED")
+    private boolean expired;
+
+    @Column(name = "IS_PHONE_VERIFIED")
+    private boolean phoneVerified;
+
+    @Column(name = "IS_EMAIL_VERIFIED")
+    private boolean emailVerified;
+
+
+
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !this.locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return !this.credentialsExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return !this.expired;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
 }
