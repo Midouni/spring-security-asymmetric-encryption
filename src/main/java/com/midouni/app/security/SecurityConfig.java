61850f6cef84fa6cd07aecf67062d7cf16180838
Midouni <mohamedmidouni2@gmail.com>
2025-10-23 20:42:40 +0100

    Add Public URLs

--- src/main/java/com/midouni/app/security/SecurityConfig.java
+++ src/main/java/com/midouni/app/security/SecurityConfig.java
@@ -4,26 +4,51 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private static final String[] PUBLIC_URLS = {
+            "/auth/login",
+            "/auth/register",
+            "/auth/refresh",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "configuration/ui",
+            "configuration/security",
+            "/swagger-ui/**",
+            "webjars/**",
+            "swagger-ui.html"
+    };
     private final AuthenticationProvider authProvider;
+    private final JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        return http.authorizeHttpRequests(
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
                 authorizeRequests-> {
-                    authorizeRequests.requestMatchers("/auth").permitAll();
+                    authorizeRequests.requestMatchers(PUBLIC_URLS).permitAll();
                     authorizeRequests.anyRequest().authenticated();
-                }
-        )
+                })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authProvider)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
