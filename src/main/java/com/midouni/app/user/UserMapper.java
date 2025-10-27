ae7272955d42cf3ab807e7500776edef1cf34b9c
Midouni <mohamedmidouni2@gmail.com>
2025-10-27 18:57:24 +0000

    Add User Mapper

--- src/main/java/com/midouni/app/user/UserMapper.java
+++ src/main/java/com/midouni/app/user/UserMapper.java
@@ -1,4 +1,16 @@
 package com.midouni.app.user;
 
+
+
+import com.midouni.app.user.request.ProfileUpdateRequest;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+    public void mergerUserInfo(User user, ProfileUpdateRequest request) {
+        user.setFirstName(StringUtils.isNotBlank(request.getFirstName()) ? request.getFirstName() : user.getFirstName());
+        user.setLastName(StringUtils.isNotBlank(request.getLastName()) ? request.getLastName() : user.getLastName());
+        user.setBirthday(request.getBirthday() != null ? request.getBirthday() : user.getBirthday());
+    }
 }
